e3d8bfa15fdec5717dfeed16f648dee9319df736
Aarkmaurya <mouryaark@gmail.com>
2026-09-08 10:02:19 +0530

    Add color definitions for theme in Color.kt

---
+++ app/src/main/java/com/nexora/kaamsetu/core/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.nexora.kaamsetu.core.theme
+
+import androidx.compose.ui.graphics.Color
+
+val KsBlue = Color(0xFF1E5AA8)
+val KsBlueDark = Color(0xFF123B72)
+val KsOrange = Color(0xFFF2994A)
+val KsBackground = Color(0xFFF7F8FA)
+val KsSurface = Color(0xFFFFFFFF)
+val KsError = Color(0xFFB3261E)
+val KsOnPrimary = Color(0xFFFFFFFF)
+val KsTextPrimary = Color(0xFF1A1C1E)
+val KsTextSecondary = Color(0xFF5F6368)
